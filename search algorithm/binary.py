1f0e62d5870b5935e2f2946b6c1c267037d80171
fuadsaft <fuadsafat16@gmail.com>
2023-08-16 15:03:26 +0600

    update binary and binary recursion

--- search algorithm/binary.py
+++ search algorithm/binary.py
@@ -10,4 +10,17 @@ def binary_search(list, target):
             first = midpoint+1
         else:
             last = midpoint-1
-    return -1
+    return None
+
+
+def verify(index):
+    if index is not None:
+        print("Target found at index: ",index)
+    else:
+        print("Target is not found in list")
+
+if __name__ == "__main__":
+    number = [1,2,3,4,5,6,7,8,9]
+    number.sort()
+    result = binary_search(number,9)
+    verify(result)
--- search algorithm/binary_recursive.py
+++ search algorithm/binary_recursive.py
@@ -13,3 +13,30 @@ def recursive_binary_search(list, target, start=0, end=None):
             return recursive_binary_search(list, target, start, mid-1)
         else:
             return recursive_binary_search(list, target, mid+1, end)
+        
+def recursive_binary_search_v1(list,target):
+    if len(list) == 0:
+        return None
+    else:
+        midpoint = (len(list))//2
+        if list[midpoint] == target:
+            return True
+        else:
+            if list[midpoint] < target:
+                return recursive_binary_search_v1(list[midpoint+1:],target)
+            else:
+                return recursive_binary_search_v1(list[:midpoint], target)
+            
+def verify(index):
+    if index is not None:
+        print("Target found")
+    else:
+        print("Target is not found in list")
+
+if __name__ == "__main__":
+    number = [1,2,3,4,5,6,7,8,9]
+    number.sort()
+    result = recursive_binary_search_v1(number,91)
+    verify(result)
+
+            
