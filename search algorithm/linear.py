8266cdb2e64c89d2d3656f4ab415781fc788f9f1
fuadsaft <fuadsafat16@gmail.com>
2023-08-16 10:07:06 +0600

    update linear

--- search algorithm/linear.py
+++ search algorithm/linear.py
@@ -1,5 +1,19 @@
 def linear_search(lst, target):
+    """
+    Return index position of target if found, else return None
+    """
     for index, value in enumerate(lst):
         if value == target:
             return index
-    return -1
+    return None
+
+def verify(index):
+    if index is not None:
+        print("Target found at index: ",index)
+    else:
+        print("Target is not found in list")
+
+if __name__ == "__main__":
+    number = [1,2,3,4,5,6,9,8,7]
+    result = linear_search(number,9)
+    verify(result)
